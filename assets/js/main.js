4d9df493703ba9cdd03db345e160db8195f154e5
aomori-mikan <shigaki1995@gmail.com>
2026-05-31 17:27:21 +0900

    Add files via upload
    データ追加

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg932\cocoartf2868
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 \
+document.addEventListener("DOMContentLoaded", () => \{\
+    const btn = document.getElementById("sampleBtn");\
+\
+    if (btn) \{\
+        btn.addEventListener("click", () => \{\
+            alert("\uc0\u12480 \u12511 \u12540 \u12469 \u12452 \u12488 \u12398 \u12469 \u12531 \u12503 \u12523 \u12391 \u12377 \u12290 ");\
+        \});\
+    \}\
+\
+    console.log("main.js loaded");\
+\});}
